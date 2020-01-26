c7f35548e3011e011f94b450e67e40952569c628
Christopher Milan <chris.m.milan@gmail.com>
2020-01-25 19:34:28 -0500

    init

---
+++ src/.temp/routes.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    name: "404",
+    path: "/404/",
+    component: () => import(/* webpackChunkName: "page--node-modules--gridsome--app--pages--404-vue" */ "/home/chris/Projects/commun/node_modules/gridsome/app/pages/404.vue")
+  },
+  {
+    name: "*",
+    path: "*",
+    component: () => import(/* webpackChunkName: "page--node-modules--gridsome--app--pages--404-vue" */ "/home/chris/Projects/commun/node_modules/gridsome/app/pages/404.vue")
+  }
+]
+
